e137e52b81a5016012afcaf59b6d161f53d5fcbb
edaersu <edaersu1@gmail.com>
2021-09-18 23:09:18 +0300

    sleepapp project created

---
+++ course2_projects/SleepApp/app/src/main/java/com/example/sleepapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.sleepapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ course2_projects/SleepApp/app/src/main/java/com/example/sleepapp/database/SleepDatabase.kt
@@ -0,0 +1,5 @@
+package com.example.sleepapp.database
+
+class SleepDatabase {
+
+}
