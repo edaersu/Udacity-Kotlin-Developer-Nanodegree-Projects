982ba6b8317d729190ab0caece16f2e82684c86b
edaersu <edaersu1@gmail.com>
2021-10-03 20:55:57 +0300

    some fix

---
+++ course3_projects/Notification/app/src/main/java/com/example/notification/receiver/AlarmReceiver.kt
@@ -0,0 +1,26 @@
+package com.example.notification.receiver
+
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import androidx.core.content.ContextCompat
+import com.example.notification.R
+import com.example.notification.util.sendNotification
+
+class AlarmReceiver: BroadcastReceiver() {
+
+    override fun onReceive(context: Context, intent: Intent) {
+        val notificationManager = ContextCompat.getSystemService(
+            context,
+            NotificationManager::class.java
+        ) as NotificationManager
+
+        notificationManager.sendNotification(
+            context.getText(R.string.eggs_ready).toString(),
+            context
+        )
+
+    }
+
+}
---
+++ course3_projects/Notification/app/src/main/java/com/example/notification/ui/EggTimerFragment.kt
@@ -0,0 +1,68 @@
+package com.example.notification.ui
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.graphics.Color
+import android.os.Build
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.notification.R
+import com.example.notification.databinding.FragmentEggTimerBinding
+
+class EggTimerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: FragmentEggTimerBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_egg_timer, container, false
+        )
+
+        val viewModel = ViewModelProvider(this).get(EggTimerViewModel::class.java)
+
+        binding.eggTimerViewModel = viewModel
+        binding.lifecycleOwner = this.viewLifecycleOwner
+
+        createChannel(
+            getString(R.string.egg_notification_channel_id),
+            getString(R.string.egg_notification_channel_name)
+        )
+
+        return binding.root
+    }
+
+    private fun createChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                channelId,
+                channelName,
+                NotificationManager.IMPORTANCE_HIGH
+            )
+                .apply {
+                    setShowBadge(false)
+                }
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = getString(R.string.breakfast_notification_channel_description)
+
+            val notificationManager = requireActivity().getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+
+        }
+    }
+
+    companion object {
+        fun newInstance() = EggTimerFragment()
+    }
+}
