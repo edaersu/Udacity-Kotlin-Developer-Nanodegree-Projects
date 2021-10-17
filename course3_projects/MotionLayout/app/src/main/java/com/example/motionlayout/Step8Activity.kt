966b488c990b4459a6aa5fadf44deb52fce859ad
edaersu <edaersu1@gmail.com>
2021-10-17 22:12:58 +0300

    created motion layout app

---
+++ course3_projects/MotionLayout/app/src/main/java/com/example/motionlayout/Step8Activity.kt
@@ -0,0 +1,14 @@
+package com.example.motionlayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Step8Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_step8)
+
+    }
+
+}
